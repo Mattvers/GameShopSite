b93936d4c33434a8833d1b23ce7ab6480729d5e0
Mateo <mateusz.grzegorzewicz@gmail.com>
2019-05-20 17:13:33 +0200

    Create variables and add photos

---
+++ app.js
@@ -0,0 +1,28 @@
+// variables
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// cart item
+let cart = [];
+
+//getting products
+class Products {}
+
+// UI - class/ display
+class UI {}
+
+// local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // creating new instanse of ui and products
+  const ui = new UI();
+  const products = new Products();
+});
